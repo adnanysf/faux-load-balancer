b1e4a21199645ad8c158c5dd0a2055ea6da26373
adnanysf <asubyusuf@gmail.com>
2025-01-05 21:41:52 +0800

    added  request class

---
+++ Requests.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string> 
+#include <vector>
+
+struct Request {
+    std::string ipIn;
+    std::string ipOut;
+    int time;
+    char jobType;
+
+    Request(std::string in, std::string out, int time, char type)
+        : ipIn(in), ipOut(out), time(time), jobType(type) {}
+};
