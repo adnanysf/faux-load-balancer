c20e9c06cb00faf8b22dd2496c755aa502b40a9d
adnanysf <asubyusuf@gmail.com>
2025-01-07 03:36:24 +0800

    add doxygen code to request.cpp

--- Requests.cpp
+++ Requests.cpp
@@ -1,14 +1,30 @@
+/**
+ * @file Request.cpp
+ * @brief Defines the Request structure for handling job requests.
+ */
+
 #include <iostream>
 #include <string> 
 #include <vector>
 #pragma once
 
+/**
+ * @struct Request
+ * @brief Represents a job request with input and output IP addresses, time, and job type.
+ */
 struct Request {
-    std::string ipIn;
-    std::string ipOut;
-    int time;
-    char jobType;
+    std::string ipIn;    /**< Input IP address */
+    std::string ipOut;   /**< Output IP address */
+    int time;            /**< Time of the request */
+    char jobType;        /**< Type of the job */
 
+    /**
+     * @brief Constructor for creating a Request object.
+     * @param in Input IP address
+     * @param out Output IP address
+     * @param time Time of the request
+     * @param type Type of the job
+     */
     Request(std::string in, std::string out, int time, char type)
         : ipIn(in), ipOut(out), time(time), jobType(type) {}
 };
